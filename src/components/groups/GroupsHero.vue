ed3f0d0c982952668d71ab82ea23312030ae2002
frontend <frontend@proton.me>
2026-05-21 21:07:32 +0300

    fix: Обновил главный экран

--- src/components/groups/GroupsHero.vue
+++ src/components/groups/GroupsHero.vue
@@ -12,11 +12,11 @@ const emit = defineEmits<{
 
 <template>
   <section class="profile-hero groups-hero">
-    <div>
-      <p class="eyebrow">Категории промокодов</p>
-      <h2>Наведи порядок в скидках</h2>
-      <p>Создавай группы для магазинов: «Магнит», «Золотое яблоко», «Аптеки» или любые свои категории.</p>
-    </div>
+<!--    <div>-->
+<!--      <p class="eyebrow">Категории промокодов</p>-->
+<!--      <h2>Наведи порядок в скидках</h2>-->
+<!--      <p>Создавай группы для магазинов: «Магнит», «Золотое яблоко», «Аптеки» или любые свои категории.</p>-->
+<!--    </div>-->
     <button class="button button--primary" type="button" :disabled="isCreating" @click="emit('create')">
       <LoadingSpinner v-if="isCreating" />
       <span>+ Создать группу</span>
